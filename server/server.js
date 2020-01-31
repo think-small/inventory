c55e665c202cfa5edc69e1932d075edd35d565d4
tmen427 <menx0003@umn.edu>
2020-01-31 12:51:20 -0600

    updates backend

--- server/server.js
+++ server/server.js
@@ -1,20 +1,31 @@
 const express = require('express');
-const apiRouter = require( './routes');
-const path = require('path')
+const path = require('path');
 
- const app = express();
+const app = express();
 
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '../public')));
 
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
 
+app.get('/api/hello', (req,res)=> {
+  res.json("hello world!")
+  console.log('hopefully this works!');
+})
 
+// Handles any requests that don't match the ones above
 
+//u need to include the below code this so that the back knows where to route and (connect with react on the front end!!!)
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
-app.use(express.static('public'));
-app.use(apiRouter.router);
+const port = process.env.PORT || 8080
+app.listen(port);
 
-
-
-
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Server listening on port: ${port}`));
+console.log('Hello, (This the server.js file speaking) App is listening on port ' + port);
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports =  {
    historyApiFallback: true,
  contentBase: path.join(__dirname + "/public"),
 
+
  // publicPath: "/dist/",
  
 
