edcd7d9a86eb614431b27902b7ab2a85735b54be
Barry Small <Barry.T.Small@gmail.com>
2020-02-04 10:41:36 -0600

    Updated build files to render most recent changes to front end.

--- src/Pages/Cobas8100.js
+++ src/Pages/Cobas8100.js
@@ -1,42 +1,24 @@
 import React from "react";
 
 class Cobas8100 extends React.Component {
-    
-    state = {
-        name: ''
-    }
-    
-    componentWillMount() {
-        fetch('http://localhost:8080/api/hello')     //not sure why you have to type in the entire route??? instead of just /api/hello
-        .then((response) => {
-          return response.json();
-        })
-        .then((myJson) => {
-          console.log(myJson);
-          this.setState({name: myJson});
-        });
+  state = {
+    name: ""
+  };
 
-
-  
-    }
-
-    render() {
-      return (
-        <div>
-          Hello {this.state.name}
-        </div>
-      );
-    }
+  componentWillMount() {
+    fetch("http://localhost:8080/api/hello") //not sure why you have to type in the entire route??? instead of just /api/hello
+      .then(response => {
+        return response.json();
+      })
+      .then(myJson => {
+        console.log(myJson);
+        this.setState({ name: myJson });
+      });
   }
-  
 
-
-
-
-
-
-
-
-
+  render() {
+    return <div>Hello {this.state.name}</div>;
+  }
+}
 
 export default Cobas8100;
