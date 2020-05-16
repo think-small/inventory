65d1e5f14c663c74d602d06648e12fdc4ff9f6fa
tmen427 <menx0003@umn.edu>
2020-05-16 18:24:55 -0500

    refactoring code in the dashboard  page

--- src/pages/dashboard/dashboard.component.js
+++ src/pages/dashboard/dashboard.component.js
@@ -21,132 +21,98 @@ import 'chartjs-plugin-datalabels'
 
 const DashboardComponent = () => {
 
-    const [database, setdatabase] = useState([]);
-    const [database1, setdatabase1] = useState([]);
-    const [database_1_name, setdatabase1_name] = useState([]);
-    const [Searchbar_value, setSearch] = useState("");
-    const [display_results, set_results] = useState([]);
-    const [ablItems, setAblItems] = useState([]);
+  
+    //name of the our tables from the database, we will get values from the fetch requests
+    const [Cobas8100, setCobas8100] = useState([]);
     const [Abl, setAbl] = useState([]);
+    const [SignIn, setSignIn] = useState([]);
+    const [Cobas8100name, setCobas8100name] = useState([]);
 
-    const current_time = moment().format("LT");
-    const current_date = moment().format("LL");
+
+    //value from the searchbar 
+    const [Searchbar_value, setSearch] = useState("");
+
+
+    //after you get results from binarysearch put values into these arrays
+    const [displayResults, set_results] = useState([]);
+    const [clearAll, setclearAll] = useState([]);
 
 
     useEffect(
         ()=> {
-
-
-            fetch("api/8100")
+                fetch("api/8100")
                 .then(response => {
                     return response.json();
                 })
                 .then(myJson => {
                     console.log(myJson);
-
-                    setdatabase1(myJson);
+                    setCobas8100(myJson);
 
                 }).catch(err => console.log(err));
 
-
-            // for the search bar
-            fetch("api/8100")
+                const fetchData1 = async () => {
+                    try {
+    
+                        const res = await fetch("/api/ABL/all-items");
+                        const data = await res.json();
+                        setAbl(data);
+    
+                    } catch (err) {
+                        throw new Error("Unable to fetch AblComponent items");
+                    }
+                };
+                fetchData1();
+                // find a better solution to this...don't want to use fetch twice....
+                fetch("api/8100")
                 .then(response => {
                     return response.json();
                 })
                 .then(myJson => {
-                    console.log(myJson);
-
-                    setdatabase1_name(myJson);
+                   console.log(myJson);
+                   setCobas8100name(myJson);
 
                 }).catch(err => console.log(err));
 
-
-            //      const fetchData = async ()=> {
-            //      const res = await fetch("/api/Cobas9", {credentials: 'include'});
-            //      res.json().then(res => setdatabase(res))
-            //      .catch(err => console.log(err));
-            //    }
-
-            // fetchData();
-
-
-            const fetchData1 = async () => {
-                try {
-
-                    const res = await fetch("/api/ABL/all-items");
-                    const data = await res.json();
-                    setAblItems(data);
-
-                } catch (err) {
-                    throw new Error("Unable to fetch AblComponent items");
+                //just gets the user name info- will change in the future 
+                const fetchData = async ()=> {
+                const res = await fetch("/api/Cobas9", {credentials: 'include'});
+                res.json().then(res => setSignIn(res))
+                .catch(err => console.log(err));
                 }
-            };
-            fetchData1();
 
-
-
+             fetchData();
         },  [],
     )
 
-
-    const low_quantity =   database1.filter(items=>items.quantity<100);
-
-    const low_quantity_size = low_quantity.length;
-    const low_quantity_Abl = ablItems.filter(items=>items.quantity<100);
-
-    const low_quantity_size_Abl = low_quantity_Abl.length;
-    console.log('the length of this is' + low_quantity_size_Abl);
-    const days_left =  database1.filter(items=>items.timeLeft<100);
-    const days_left_size = days_left.length;
-
-
-    const handleSubmit = event=> {
-        // alert('the value of your search is' + Searchbar_value);
-        binarySearch(database1, Searchbar_value,"Lot");
-        binarySearch(database_1_name, Searchbar_value, "Name");
-        binarySearch(ablItems, Searchbar_value, "lotNum");
-        event.preventdefault;
-    }
-
+//when you sort the arrays...
     const handleChange = event => {
         const value = event.target.value;
         const target = event.target.type
-
-        if (target==="text") {
+         if (target==="text") {
             setSearch(value);
         }
-// When using binary search the objects must first be sorted alphabetically
-        function Alphabetical_Sort(database_name,Object_Property) {
-            database_name.sort(function(a, b) {
-                var nameA = a[Object_Property]   //ABCDEF...Z is greator than abcdefg in javascript!!
-                var nameB = b[Object_Property]
-                if (nameA < nameB) {
-                    // push the entire object into the array, but now it is ordered
 
-                    return -1;
-                }
-                if (nameA > nameB) {
-
-                    return 1;
-                }
-                // names must be equal
-                return 0;
-
+        function Alphabetical_Sort(databaseName,Object_Property) {
+            databaseName.sort(function(a, b) {
+            var nameA = a[Object_Property]   //ABCDEF caps come before lowercase (abcdef) in javascript!!
+            var nameB = b[Object_Property]
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+    
+                return 1;
+            }
+            return 0;
             });
         }
 
-        Alphabetical_Sort(database1, "Lot");
-        Alphabetical_Sort(ablItems, "lotNum");
-        Alphabetical_Sort(database_1_name, "Name");
-        console.log( database1)
-        console.log(ablItems);
-        console.log( database_1_name)
+        Alphabetical_Sort(Cobas8100, "lotNum");
+        Alphabetical_Sort(Abl, "lotNum");
+        Alphabetical_Sort(Cobas8100name, "displayName");
+       }
 
-
-    }
-
-// for our serach we our using Binary Search
+//defining binary serach algorithim       
     const binarySearch = (array, target, Lot) => {
         let startIndex = 0;
         let endIndex = array.length -1;
@@ -163,45 +129,46 @@ const DashboardComponent = () => {
                 if (array[middleIndex].Name) {
                     set_results([array[middleIndex]])
                     //clear the other array
-                    setAbl([]);
+                    setclearAll([]);
                 }
                 //seperate here, why the arrays are different
                 if (array[middleIndex].displayName) {
-                    setAbl([array[middleIndex]])
+                    setclearAll([array[middleIndex]])
                     //clear the other array
                     set_results([])
                 }
 
-//check for duplicates by doing a linear search, the maximum number of iterations is array.length / 2
+                //check for duplicates by doing a linear search, the maximum number of iterations is array.length / 2
 
                 var array_size = Math.floor((array.length) / 2);
                 var copy = [];
-//for the right hand side
+                //for the right hand side
                 for (var counter =1; counter <=array_size; counter++) {
                     var add = middleIndex+counter;
                     if (target === array[add][Lot]) {
 
                         copy.push(array[add]);
 
-                        set_results(display_results => {
-                            return  ([...display_results, ...copy]);
+                        set_results(displayResults => {
+                            return  ([...displayResults, ...copy]);
                         });
                     }
                 }
-//for the left hand side
+                //for the left hand side
                 for (var counter =1; counter <= array_size; counter++) {
                     var subtract = middleIndex-counter;
                     if (target === array[subtract][Lot]) {
 
                         copy.push(array[subtract]);
                         // console.log("ffis " + ff + " " + target + "  " + counter + " " + array[middleIndex+counter][Lot] + array[middleIndex+counter].Lot)
-                        set_results(display_results => {
-                            return  ([...display_results, ...copy]);
+                        set_results(displayResults => {
+                            return  ([...displayResults, ...copy]);
                         });
                     }
                 }
 
                 return console.log("Target was found at index " + middleIndex);
+                
             }
 
 
@@ -216,37 +183,47 @@ const DashboardComponent = () => {
             }
             else {
                 console.log("Not Found this loop iteration. Looping another iteration.")
+            
             }
         }
         console.log("Target value not found in array");
     }
 
+    const low_quantity =   Cobas8100.filter(items=>items.quantity<100);
+    const low_quantity_size = low_quantity.length;
+    
+    const low_quantity_Abl = Abl.filter(items=>items.quantity<100);
+    const low_quantity_size_Abl = low_quantity_Abl.length;
+ 
+    const days_left =  Cobas8100.filter(items=>items.timeLeft<100);
+    const days_left_size = days_left.length;
 
+    const handleKeyPress = (target)=> {
 
-    function handleKeyPress(target) {
         if(target.charCode==13){
-            //  alert('the vlaue of your search is' + Searchbar_value);
-            //call binarysearch function
-            binarySearch(database1, Searchbar_value, "Lot");
-            binarySearch(database_1_name, Searchbar_value, "Name");
-            binarySearch(ablItems, Searchbar_value, "lotNum");
+            set_results([]); //clear the previous search 
+            binarySearch(Cobas8100, Searchbar_value, "lotNum");
+            binarySearch(Cobas8100name, Searchbar_value, "displayName");
+            binarySearch(Abl, Searchbar_value, "lotNum");
         }
     }
 
+    const handleSubmit = event=> {
+        binarySearch(Cobas8100, Searchbar_value,"lotNum");
+        binarySearch(Cobas8100name, Searchbar_value, "displayName");
+        binarySearch(Abl, Searchbar_value, "lotNum");
+        event.preventdefault;
+    }
 
-//console.log('the length of the array is' + display_results.length);
-//display the search results
-    const x = display_results.map(item=><div style={{padding: "30px"}}><div>Lot: {item.Lot}</div><div>Name: {item.Name}</div><div>Quantity: {item.Quantity}</div>
-        <div>Expiration Date: {item.Expiration_Date}</div><div>Count Per Box: {item.countPerBox}</div></div>);
-
-    const r = Abl.map(item=><div style={{padding: "30px"}}><div>Lot: {item.lotNum}</div><div>Name: {item.displayName}</div><div>Quantity: {item.quantity}</div>
+    //display the search results
+    const searchResults = displayResults.map(item=><div style={{padding: "30px"}}><div>Lot: {item.lotNum}</div><div>Name: {item.displayName}</div><div>Quantity: {item.quantity}</div>
         <div>Expiration Date: {item.expirationDate}</div><div>Count Per Box: {item.countPerBox}</div></div>);
 
-    var Lots= low_quantity.map(item=>item.lotNum);
-    var Amount =   low_quantity.map(item=>item.quantity);
-    //get database Lot# and push into and array
+    const searchResults1 = clearAll.map(item=><div style={{padding: "30px"}}><div>Lot: {item.lotNum}</div><div>Name: {item.displayName}</div><div>Quantity: {item.quantity}</div>
+        <div>Expiration Date: {item.expirationDate}</div><div>Count Per Box: {item.countPerBox}</div></div>);
 
 
+// info for the pie graph 
     const graph_data1 = {
 
         labels: ["Low Quantity", "About to Expire"] ,
@@ -261,24 +238,14 @@ const DashboardComponent = () => {
         ]
     }
 
-
-
-
     return (
         <div>
 
             <NavbarComponent/>
-            {database.length === 1 ? <h1 style={{padding:"30px"}}> {   database.map(item=> <div>Hello, {item.Username}</div>)} </h1> : <h1></h1>      }
+            {SignIn.length === 1 ? <h1 style={{padding:"30px"}}> {   SignIn.map(item=> <div>Hello, {item.Username}</div>)} </h1> : <h1></h1>      }
 
 
-
-
-
-
-
-            {/*
-
-<div className="searchMenu">
+        <div className="searchMenu">
             <div className="fas">
               <i
                 className="fas fa-search"
@@ -294,30 +261,24 @@ const DashboardComponent = () => {
               value={Searchbar_value} onChange={handleChange} 
               onKeyPress={handleKeyPress}
             />
-</div>
-
-<div>{x}</div>
-<div>{r}</div>
+        </div>
 
 
-*/}
+{searchResults.length>=1 ? <div> {searchResults} </div>: <div>  </div>}
+{searchResults1.length>=1 ? <div> {searchResults1} </div>: <div>  </div>}
+
+
+
 
             <div style={{padding: "25px" , marginLeft: "20px",  fontSize:"30px"}}> </div>
-
-
-
-
             <Container >
                 <Row>  {/*
     <Col xs={6} md={4}>
-
     <Card style={{ width: '20rem' }} bg="danger">
-  <Card.Header>Total Warnings: {low_quantity_size+days_left_size+low_quantity_size_Abl}</Card.Header>
-  <ListGroup variant="flush">
- 
-
-<ListGroup.Item>
-<Doughnut
+    <Card.Header>Total Warnings: {low_quantity_size+days_left_size+low_quantity_size_Abl}</Card.Header>
+    <ListGroup variant="flush">
+    <ListGroup.Item>
+    <Doughnut
           data={graph_data1}
           options={{
             title:{
@@ -334,7 +295,7 @@ const DashboardComponent = () => {
           }}
         />
 
-</ListGroup.Item>
+    </ListGroup.Item>
     <ListGroup.Item style={{color:"blue"}}>Low Quantity Total: {low_quantity_size+low_quantity_size_Abl} </ListGroup.Item>
     <ListGroup.Item style={{color:"pink"}}>Days Left Total: {days_left_size} </ListGroup.Item>
 </ListGroup>
@@ -525,75 +486,12 @@ const DashboardComponent = () => {
                             </ListGroup>
                         </Card>
                     </Col>
-
-
-
-
-
-                </Row>
+               </Row>
             </Container>
 
 
 
-            {/*
-<div style={{width: "70%",  padding: "30px"}}>
-<Tabs defaultActiveKey="abl" id="uncontrolled-tab-example">
-  <Tab eventKey="abl" title="Abl" style={{color: "yellow"}}>
-  
-  <Table striped bordered hover>
-  <thead>
-    <tr>
-      <th>Lot #</th>
-      <th>Quantity</th>
-      <th>Expiration Date</th>
-      
-    </tr>
-  </thead>
-  <tbody>
-   {low_quantity_Abl.map(item=>   <tr> <td>{item.lotNum}   </td>  
-   <td>{item.quantity}</td>
-   <td>{item.expirationDate}</td>
-       </tr>
-     
-     )} 
-</tbody>
-
-</Table>
-
-  </Tab>
-  <Tab eventKey="Architect" title="Architect">
-    <div>Nothin yet</div>
-  </Tab>
-  <Tab eventKey="Cobas 8000" title="Cobas 8000">
-   <div>Nothin yet </div>
-  </Tab>
-  <Tab eventKey="Cobas 8100" title="Cobas 8100">
-
-  <Table striped bordered hover>
-  <thead>
-    <tr>
-      <th>Lot #</th>
-      <th>Quantity</th>
-      <th>Expiration Date</th>
-      
-    </tr>
-  </thead>
-  <tbody>
-   {low_quantity.map(item=>   <tr> <td>{item.Lot}   </td>  
-   <td>{item.Quantity}</td>
-   <td>{item.Expiration_Date}</td>
-       </tr>
-     
-     )} 
-</tbody>
-
-</Table>
-
-</Tab>
-</Tabs>
-</div>
-     
-   */}
+ 
 
         </div>
 
